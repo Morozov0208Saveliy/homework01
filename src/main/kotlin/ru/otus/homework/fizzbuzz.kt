4ba22dc972241555de4bd7edde199a88a000689c
Morozov0208Saveliy <savelmor@yandex.ru>
2024-04-08 18:50:03 +0200

    Попытка #1

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -2,5 +2,18 @@ package ru.otus.homework
 
 
 fun fizzbuzz(n: Int): Array<String> {
-    TODO("Выполните задание")
+    val startNumber = 0
+    val array = ArrayList<String>(n)
+    for (i in startNumber..n - 1) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            array.add("FizzBuzz")
+        } else if (i % 5 == 0) {
+            array.add("Buzz")
+        } else if (i % 3 == 0) {
+            array.add("Fizz")
+        } else {
+            array.add(i.toString())
+        }
+    }
+    return array.toTypedArray()
 }
